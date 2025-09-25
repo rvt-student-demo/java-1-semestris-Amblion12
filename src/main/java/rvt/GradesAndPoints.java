1d64a1771d9d1de9ea722c8f0f199ca12eca3713
Ēriks Vansovičs <ericvanlocked@gmail.com>
2025-09-25 10:56:40 +0300

    Fix conditional logic in GradesAndPoints class for grade evaluation

--- src/main/java/rvt/GradesAndPoints.java
+++ src/main/java/rvt/GradesAndPoints.java
@@ -8,24 +8,23 @@ public class GradesAndPoints {
         System.out.println("Ievadiet ballus: ");
     String myNum = myObj.nextLine();
     int num = Integer.parseInt(myNum);
-    if (num <= 0 && num >= 49) {
+   if (num >= 0 && num <= 49) {
         System.out.println("Failed");
-            } else if (num <= 50 && num >= 59) {
+    } else if (num >= 50 && num <= 59) {
         System.out.println("1");
-            } else if (num <= 60 && num >= 69) {
+    } else if (num >= 60 && num <= 69) {
         System.out.println("2");
-            } else if (num <= 70 && num >= 79) {
+    } else if (num >= 70 && num <= 79) {
         System.out.println("3");
-            } else if (num <= 80 && num >= 89) {
+    } else if (num >= 80 && num <= 89) {
         System.out.println("4");
-            } else if (num <= 90 && num >= 100) {
+    } else if (num >= 90 && num <= 100) {
         System.out.println("5");
     } else if (num > 100) {
         System.out.println("Incredible.");
+    } else {
+        System.out.println("Impossible!");
     }
-        else {
-            System.out.println("Impossible!");
-        }
     myObj.close();
   }
 }
