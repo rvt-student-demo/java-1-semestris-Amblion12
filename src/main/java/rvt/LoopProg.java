e8b85f16a24f8e6b54077d1e5e08c92fca8b6ab9
Ēriks Vansovičs <ericvanlocked@gmail.com>
2025-10-06 08:53:36 +0300

    Add LoopProg class with initial setup for user input processing

---
+++ src/main/java/rvt/LoopProg.java
@@ -0,0 +1,18 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class LoopProg {
+    public static void main(String[] args) {
+       int skaisk = 0;
+       int skaisum = 0;
+       int skaivid = 0;
+       int skaimax = 0;
+       int skaimin = 0;
+       Scanner myObj = new Scanner(System.in);
+       while (myObj > 0) {
+
+       }
+
+    }
+}
