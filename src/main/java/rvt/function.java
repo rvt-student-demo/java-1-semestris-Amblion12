5e4b159e3274d9540a95b2908e8a03f1e3649ab2
Ēriks Vansovičs <ericvanlocked@gmail.com>
2025-10-10 12:01:19 +0300

    Mana pirma funkcija

---
+++ src/main/java/rvt/function.java
@@ -0,0 +1,18 @@
+package rvt;
+
+public class function {
+    public static void main(String[] args) {
+
+        System.out.println("AAA");
+        greet();
+
+        System.out.println("Looks great");
+        greet();
+        greet();
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("BBBB");
+    }
+}
