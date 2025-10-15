04f82012fc7b380f8903bf2e489be5f05de86952
Ēriks Vansovičs <ericvanlocked@gmail.com>
2025-10-15 10:29:57 +0300

    Mana pirma programma ar masīviem

---
+++ src/main/java/rvt/Array1.java
@@ -0,0 +1,11 @@
+package rvt;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int[] cars = {1, 13, 13, 421, 3};
+
+        for (int x = 0; x < cars.length; x++) {
+            System.out.println(cars[x]);
+        }
+    }
+}
