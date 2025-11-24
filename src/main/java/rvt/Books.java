917b2f817c162703e7aa689f199c1705abf7492f
Ēriks Vansovičs <ericvanlocked@gmail.com>
2025-11-24 10:42:26 +0200

    Books exercises

--- src/main/java/rvt/Books.java
+++ src/main/java/rvt/Books.java
@@ -1,6 +1,8 @@
 package rvt;
+import java.util.Scanner;
 
 public class Books {
+    Scanner scanner = new Scanner(System.in);
     // 1. Klasi kura raksturos grāmatas
 
     // 2. Izveidosim tukšu sarakstu ar grāmatam
@@ -8,6 +10,11 @@ public class Books {
     // ArrayList<Book>
     while (true) {
         // Lietotājs ievadīs grāmatas
+        System.out.print("Title: ");
+
+        System.out.print("Pages: ");
+
+        System.out.print("Publication year: ");
         // Ievietosiet tās grāmatas sarakstā
     }
     // Pajautāt lietotājam kā būs printēta info
