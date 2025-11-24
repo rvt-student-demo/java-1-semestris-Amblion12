0b873de1504658c6ad0e1bffcd2ab02f0592187b
Ēriks Vansovičs <ericvanlocked@gmail.com>
2025-11-24 10:48:18 +0200

    Book uzd

--- src/main/java/rvt/Books.java
+++ src/main/java/rvt/Books.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 public class Books {
     Scanner scanner = new Scanner(System.in);
     // 1. Klasi kura raksturos grāmatas
-
+    private String title;
+    private int pages;
+    private int PY; 
     // 2. Izveidosim tukšu sarakstu ar grāmatam
-
     // ArrayList<Book>
     while (true) {
         // Lietotājs ievadīs grāmatas
