1ca6d0dcb62ebfb7f6665f49a6161d8aa2f72a92
Ēriks Vansovičs <ericvanlocked@gmail.com>
2025-11-24 10:36:33 +0200

    Books exercise

---
+++ src/main/java/rvt/Books.java
@@ -0,0 +1,21 @@
+package rvt;
+
+public class Books {
+    // 1. Klasi kura raksturos grāmatas
+
+    // 2. Izveidosim tukšu sarakstu ar grāmatam
+
+    // ArrayList<Book>
+    while (true) {
+        // Lietotājs ievadīs grāmatas
+        // Ievietosiet tās grāmatas sarakstā
+    }
+    // Pajautāt lietotājam kā būs printēta info
+
+    // Ir divas opcijas: everything vai name
+
+    //Printējam katru grāmatu no saraksta
+
+
+    // String vērtības mēs salidzinam ar equals() metodi
+}
